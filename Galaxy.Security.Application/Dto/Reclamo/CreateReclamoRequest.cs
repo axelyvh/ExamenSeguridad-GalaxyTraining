af532a8da793d17ddf592958cb56e06bfe5c522a
avasquezh-maquimas <avasquezh@maquimas.pe>
2025-10-23 22:03:14 -0500

    init

---
+++ Galaxy.Security.Application/Dto/Reclamo/CreateReclamoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Galaxy.Security.Application.Dto.Reclamo
+{
+    public class CreateReclamoRequest
+    {
+        [Required(ErrorMessage = "El campo Descripcion es obligatorio")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El campo Fecha es obligatorio")]
+        public DateTime Fecha { get; set; }
+    }
+}
